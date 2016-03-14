7267da465304c2f6ee4f4385291faaa553274a62
rohithrangaraju <rohithrangaraju@missionrnd.com>
2016-03-14 20:09:02 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -13,7 +13,7 @@ ERROR CASES: Return NULL for invalid inputs.
 
 NOTES:
 */
-
+#include<malloc.h>
 struct transaction {
 	int amount;
 	char date[11];
@@ -21,5 +21,33 @@ struct transaction {
 };
 
 int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+	int i,j,k=0,max=0,count=0,n=1000;
+	for (i = 6; i < 10; i++){
+		if (date[i] != '-')
+		max = max+((date[i])-'0')*n;
+		n = n / 10;
+	}
+	int *Add;
+	Add = (int*)calloc(5, sizeof(int));
+	for (i = 0; i<len; i++){
+		n = 1000;
+		for (j = 6; j<10; j++){
+			if (Arr[i].date[j] != '-')
+				Add[k] = Add[k] + ((Arr[i].date[j]) - '0')*n;
+			n = n / 10;
+		}
+		k++;
+	}
+	for (i = 0; i < len; i++){
+		if (max == Add[i]) count++;
+	}
+	if (count != 0){
+		count = 0;
+		for (i = 0; i < len; i++){
+			if (max < Add[i])count++;
+		}
+		return count;
+	}
+	else
+		return count;
 }
